fc095db72a7a764f4de73fcbda60b6aa4942a2be
Manali Darji <darjimanali93@gmail.com>
2022-08-30 23:11:31 -0400

    refactor: update project item on project lists page

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,20 @@
 import { Link } from "react-router-dom";
 
 const ProjectItem = (props) => {
-    const repo = props.repo;
+  const project = props.project;
   return (
-    <div>
-        <Link to={`projects/${repo.name}`}>{repo.name}</Link>
+    <div className="col-lg-4 col-md-6 py-3">
+        <Link to={`projects/${project.name}`} className="card-a">
+            <div className="card text-center">
+                <h4 className="card-header mb-2 py-3">{project.name}</h4>
+                <div className="card-body">
+                    <p className="card-text">{project.description}</p>
+                    <button className="btn btn-dark">View Project Details</button>
+                </div>
+            </div>
+        </Link>
     </div>
-  )
-}
+  );
+};
 
-export default ProjectItem
+export default ProjectItem;
