1e41880311ad31243f912b55ef0fc91bc779bdbc
Manali Darji <darjimanali93@gmail.com>
2022-08-31 00:14:06 -0400

    refactor: updated repo created date

--- src/components/Projects/ProjectItem.js
+++ src/components/Projects/ProjectItem.js
@@ -2,13 +2,15 @@ import { Link } from "react-router-dom";
 
 const ProjectItem = (props) => {
   const project = props.project;
+  const repoCreationDate = new Date(project.created_at).toLocaleDateString();
   return (
     <div className="col-lg-4 col-md-6 py-3">
         <Link to={`commits/${project.name}`} className="card-a">
             <div className="card text-center">
                 <h4 className="card-header mb-2 py-3">{project.name}</h4>
                 <div className="card-body">
-                    <p className="card-text">{project.description}</p>
+                    <p className="card-text text-secondary">{project.description}</p>
+                    <p className="card-text text-secondary"><em>Repo Created on: <br/> {repoCreationDate}</em></p>
                     <button className="btn btn-dark">View Commits</button>
                 </div>
             </div>
