f9b2910635b9ea25f0941685372dba8b1abc9f36
Manali Darji <darjimanali93@gmail.com>
2022-08-31 00:14:26 -0400

    refactor: updated project commit item

--- src/components/Commits/ProjectCommitItem.js
+++ src/components/Commits/ProjectCommitItem.js
@@ -1,8 +1,19 @@
 const ProjectCommitItem = (props) => {
-    const commit = props.commit.commit;
+  const commit = props.commit.commit;
+  const commit_URL = props.commit.html_url;
   return (
-    <div>{commit.message}</div>
-  )
-}
+    <li className="list-group-item">
+      <a href={commit_URL} className="card-a" target="_blank" rel="noreferrer">
+        <h4 className="card-title mb-2 py-3">{commit.message}</h4>
+        <p className="card-text">
+          <em>
+            Commit By: {commit.author.name}
+          </em>
+        </p>
+        <button className="btn btn-dark">View Commit on Git</button>
+      </a>
+    </li>
+  );
+};
 
-export default ProjectCommitItem
+export default ProjectCommitItem;
