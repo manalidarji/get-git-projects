3542c99c6263188945d6721c1f60fc3c35f3f54f
Manali Darji <darjimanali93@gmail.com>
2022-09-01 11:31:59 -0400

    refactor: removed hardcoded git username

--- src/components/Projects/ProjectsLists.js
+++ src/components/Projects/ProjectsLists.js
@@ -1,32 +1,43 @@
 import { useState, useEffect } from "react";
 import ProjectItem from "./ProjectItem";
 
-const ProjectsLists = () => {
-	// 2 hardcoded username samples foe testing purposes
-	const username = 'manalidarji';
-	// const username = 'fatemehabedin2';
+const ProjectsLists = (props) => {
+  const [projects, setProjects] = useState([]);
+  const username = props.username;
+  const GIT_REPOS_URL = `https://api.github.com/users/${username}/repos?sort=created`;
 
-	
-	const GIT_REPOS_URL = `https://api.github.com/users/${username}/repos?sort=created`;
+  useEffect(() => {
+    fetch(GIT_REPOS_URL)
+      .then((resp) => resp.json())
+      .then((data) => setProjects(data))
+      .catch((error) => console.log(error));
+  }, [GIT_REPOS_URL]);
 
-	const [projects, setProjects] = useState([]);
+  let projectsListsTitleHTML = "";
+  if (projects.length > 0) {
+    projectsListsTitleHTML = `Below is the list of projects for: '${username}'`;
+  } else {
+    projectsListsTitleHTML = `No projects found for: '${username}'`;
+  }
 
-	useEffect(() => {
-		fetch(GIT_REPOS_URL)
-        .then(resp => resp.json())
-        .then(data => setProjects(data))
-        .catch(error => console.log(error));
-	}, [GIT_REPOS_URL]);
-
-  return(
-	<div className="row">
-		{projects.map(project => (
-			<ProjectItem
-				key = {project.id}
-				project = {project}
-			/>
-		))}
-	</div>
+  return (
+    <div>
+      <h2 className="text-center">{projectsListsTitleHTML}</h2>
+      <h6 className="text-center text-secondary mb-5">
+        Please Note: Projects are sorted by creation date.
+      </h6>
+      <div className="row">
+        {projects.length > 0
+          ? projects.map((project) => (
+              <ProjectItem
+                key={project.id}
+                project={project}
+                username={username}
+              />
+            ))
+          : ""}
+      </div>
+    </div>
   );
 };
 
